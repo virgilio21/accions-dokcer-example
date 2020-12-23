a90968702f961243e3adaf6217bcbaa6d4969b8f
Virgilio Padron Batun <virgilio.padron.sop.j@gmail.com>
2020-12-23 15:57:21 -0600

    cambio asignacion variable entrypoi

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -l
-
+NAME = "$1"
 sh -c "echo my name is: $INPUT_NAME"
 sh -c "echo my name is: $1"
 echo "my name is: $1"
+echo "$NAME"
+echo "${NAME}"
+
 
 sh -c "echo my second name is: $INPUT_SECONDNAME"
 sh -c "echo my second name is: $2"
