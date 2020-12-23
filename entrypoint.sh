3f104f67bad95460a99cf7696a720750153b748c
Virgilio Padron Batun <virgilio.padron.sop.j@gmail.com>
2020-12-23 16:03:00 -0600

    gg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh -l
 
 NAME="$1"
+NAME_NEW="$INPUT_NAME"
 sh -c "echo my name is: $INPUT_NAME"
 sh -c "echo my name is: $1"
 echo "my name is: $1"
 echo "HH $NAME"
 echo "HH ${NAME}"
+echo "NEW NAME: $NAME_NEW"
+echo "NEW NAME: ${NAME_NEW}"
 
 
 sh -c "echo my second name is: $INPUT_SECONDNAME"
