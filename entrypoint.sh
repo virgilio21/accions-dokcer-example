ff4eeaa8f4ffaae4bf48c9dceb6ce3d83b7e393b
Virgilio Padron Batun <virgilio.padron.sop.j@gmail.com>
2020-12-23 15:45:42 -0600

    cambios action y entrypoint con distintas formas

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -l
 
-NAME="$INPUT_WHO-TO-GREET"
-echo "Hello $NAME"
+sh -c "echo my name is: $INPUT_NAME"
+sh -c "echo my name is: $1"
+echo "my name is: $1"
+
+sh -c "echo my second name is: $INPUT_SECONDNAME"
+sh -c "echo my second name is: $2"
+echo "my second name is: $2"
 time=$(date)
-sh -c "echo my name is: $INPUT_WHO-TO-GREET"
 echo "::set-output name=time::$time"
